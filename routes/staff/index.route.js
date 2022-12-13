0f45d09be21021a8fd077970e879ad9dcdea75e9
hieptrinh2002 <hieptrinh2002@gmail.com>
2022-12-13 15:21:40 +0700

    update13.12

--- routes/staff/index.route.js
+++ routes/staff/index.route.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/staff/index.controller')
-const menuRoute = require('./thuc_don.route')
+
+
 //router.get('/',controller.(Trang_chu));
 
-router.use('/thuc_don',menuRoute); // xong
+//router.use('/thuc_don',menuRoute); // xong
+router.get('/thuc_don',controller.get_menu); // xong
+router.post('/thuc_don/them_mon_an', controller.post_add_food)// xong
+router.get('/thuc_don/them_mon_an', controller.get_add_food);
+
 
 
 //router.get('/kho_hang',controller.(kho_hang));
+
+
 //router.get('/khach_hang',controller.(khach_hang)); // nạp tiền ....
+router.post('/tao_the', controller.post_Create_Card);//xong
+
 //router.get('/thong_ke',controller.(thong_ke)); // theo tuần, theo tháng
 //router.get('/nhan_vien',controller.(nhan_vien));//chỉ hiển thị ra
-router.post('/tao_the', controller.post_Create_Card);//xong
+
 
 //router.post('/thanh_toan',controller.(thanh_toan));
 
